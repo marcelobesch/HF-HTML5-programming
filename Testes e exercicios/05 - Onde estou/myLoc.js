b275156e36cd13c66503bfae5c900ffc2ef4a098
marcelobesch <marcelobesch@gmail.com>
2016-03-14 19:07:16 -0300

    Terminei o geoloc, antes de ir pro mapa

--- Testes e exercicios/05 - Onde estou/myLoc.js
+++ Testes e exercicios/05 - Onde estou/myLoc.js
@@ -2,7 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation() {
 	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(displayLocation);
+		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
 	} else {
 		alert("Oops, no geolocation support");
 	}
@@ -15,4 +15,52 @@ function displayLocation(position) {
 	var div = document.getElementById("location");
 	div.innerHTML = "You are at latitude: " + latitude 
 	+ " and longitude: " + longitude;
+
+	var WSHQcoords = {
+		latitude: 47.624851,
+		longitude: -122.52099
+	};
+
+	var distance = computeDistance(position.coords, WSHQcoords);
+
+	div2 = document.getElementById("distance");
+	div2.innerHTML = "Distancia pro HQ da WS: " + distance;
+
 }
+
+function displayError(error) {
+	errorTypes = ["Unknown error", "Permission denied by user", 
+	"Position is not available", "Request timed out"];
+	var errorMessage = errorTypes[error.code];
+	if (error.code == 0 || error.code == 2) {
+		errorMessage = errorMessage + " " + error.message;
+	}
+	var div = document.getElementById("location");
+	div.innerHTML = errorMessage;
+}
+
+// --------------------- Ready Bake ------------------
+//
+// Uses the Spherical Law of Cosines to find the distance
+// between two lat/long points
+//
+function computeDistance(startCoords, destCoords) {
+	var startLatRads = degreesToRadians(startCoords.latitude);
+	var startLongRads = degreesToRadians(startCoords.longitude);
+	var destLatRads = degreesToRadians(destCoords.latitude);
+	var destLongRads = degreesToRadians(destCoords.longitude);
+
+	var Radius = 6371; // radius of the Earth in km
+	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
+					Math.cos(startLatRads) * Math.cos(destLatRads) *
+					Math.cos(startLongRads - destLongRads)) * Radius;
+
+	return distance;
+}
+
+function degreesToRadians(degrees) {
+	radians = (degrees * Math.PI)/180;
+	return radians;
+}
+
+// ------------------ End Ready Bake -----------------
