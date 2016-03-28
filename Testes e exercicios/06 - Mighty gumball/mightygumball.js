e941780bf23fb88f67c8c608fd86c805d9b6410f
marcelobesch <marcelobesch@gmail.com>
2016-03-28 20:18:25 -0300

    meados do cap 6
    Terminei o primeiro que funciona pegando o sales.json

---
+++ Testes e exercicios/06 - Mighty gumball/mightygumball.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+
+	var url = "http://marcelobesch.esy.es/sales.json";
+	var request = new XMLHttpRequest();
+	request.open("GET", url);
+	request.onload = function(){
+		if(request.status == 200) {
+			updateSales(request.responseText);
+		}
+	};
+
+	request.send(null);
+
+}
+
+function updateSales(responseText) {
+	salesDiv = document.getElementById("sales");
+	var sales = JSON.parse(responseText);
+
+	for (var i=0; i < sales.length; i++){
+		var div = document.createElement("div");
+		div.setAttribute("class","saleItem");
+		div.innerHTML = sales[i].name + " vendeu " + sales[i].sales + " gumballs.";
+		salesDiv.appendChild(div);
+	}
+}
