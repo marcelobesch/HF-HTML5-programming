fa16ed9ad78a4c8d3f6a23178d7dea0f3af290a4
marcelobesch <marcelobesch@gmail.com>
2016-03-30 18:48:09 -0300

    Meu jeito de obter as vendas do gumball
    Estou salvando o que fiz antes de ler a parte de JSONP no livro. Fiz
    usando script src no head do html mesmo. Dei um jeito de funcionar.
    Agora vou aprender o certo.

--- Testes e exercicios/06 - Mighty gumball/mightygumball.js
+++ Testes e exercicios/06 - Mighty gumball/mightygumball.js
@@ -1,26 +1,48 @@
+var vendas;
+
 window.onload = function () {
 
-	var url = "http://marcelobesch.esy.es/sales.json";
-	var request = new XMLHttpRequest();
-	request.open("GET", url);
-	request.onload = function(){
-		if(request.status == 200) {
-			updateSales(request.responseText);
+	// var url = "http://gumball.wickedlysmart.com/";
+	// var request = new XMLHttpRequest();
+	// request.open("GET", url);
+	// request.onload = function(){
+	// 	if(request.status == 200) {
+	// 		updateSales(request.responseText);
+	// 	}
+	// };
+
+	// request.send(null);
+
+	// Para JSONP:
+	updateSales(vendas);
+
+}
+
+// ComXMLHTTPRequest:
+
+// function updateSales(responseText) {
+// 	salesDiv = document.getElementById("sales");
+// 	var sales = JSON.parse(responseText);
+
+// 	for (var i=0; i < sales.length; i++){
+// 		var div = document.createElement("div");
+// 		div.setAttribute("class","saleItem");
+// 		div.innerHTML = sales[i].name + " vendeu " + sales[i].sales + " gumballs.";
+// 		salesDiv.appendChild(div);
+// 	}
+
+// Com JSONP:
+
+	function updateSales(sales) {
+		vendas = sales;
+		salesDiv = document.getElementById("sales");
+
+		if (salesDiv != null){
+			for (var i=0; i < sales.length; i++){
+				var div = document.createElement("div");
+				div.setAttribute("class","saleItem");
+				div.innerHTML = sales[i].name + " vendeu " + sales[i].sales + " gumballs.";
+				salesDiv.appendChild(div);
+			}
 		}
-	};
-
-	request.send(null);
-
-}
-
-function updateSales(responseText) {
-	salesDiv = document.getElementById("sales");
-	var sales = JSON.parse(responseText);
-
-	for (var i=0; i < sales.length; i++){
-		var div = document.createElement("div");
-		div.setAttribute("class","saleItem");
-		div.innerHTML = sales[i].name + " vendeu " + sales[i].sales + " gumballs.";
-		salesDiv.appendChild(div);
 	}
-}
