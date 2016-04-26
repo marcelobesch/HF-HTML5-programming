69bbad7d4d458add63cd5fd8dde7f93dac0dbdd7
marcelobesch <marcelobesch@gmail.com>
2016-04-25 21:21:59 -0300

    Prosseguindo com o canvas
    fiz os formulários no html

--- Testes e exercicios/08 - Canvas/canvas.js
+++ Testes e exercicios/08 - Canvas/canvas.js
@@ -1,6 +1,12 @@
 window.onload = function() {
 	var canvas = document.getElementById("meucanvas");
-	var context = canvas.getContext("2d");
-	context.fillRect(10, 10, 100, 100);
+	if(!canvas.getContext){
+		alert("Seu browser não suporta o recurso canvas");
+	}else{
+		var context = canvas.getContext("2d");
+		context.fillRect(10, 10, 100, 100);
+	}
 
+	
+	
 }
